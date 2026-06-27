52fcfa7f440eacb0a467e1067363209aa8f1aa4d
Samarpit <samarpitrawat97@gmail.com>
2026-06-27 21:54:06 +0530

    feat: add CORS support to AuthenticationController

--- src/main/java/com/campusflow/backend/controller/AuthenticationController.java
+++ src/main/java/com/campusflow/backend/controller/AuthenticationController.java
@@ -6,6 +6,7 @@ import com.campusflow.backend.dto.RegisterRequest;
 import com.campusflow.backend.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin; // 1. Import this
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000") // 2. Add this line to allow your React app
 public class AuthenticationController {
 
     private final AuthenticationService service;
